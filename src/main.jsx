acd3c61763c39ebe67bdeced4edb233ef635d2d0
shasirasu <shasirasu@gmail.com>
2026-08-03 11:48:20 +0530

    versel

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next"
 import App from "./App.jsx";
 import "./styles.css";
 
